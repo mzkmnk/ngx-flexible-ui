e8e102b31a6fc91f2912ca5e137ceea2e24d062a
mzkmnk <sma0729mex@gmail.com>
2025-02-17 07:46:10 +0900

    chore: remove commented code from release configuration

--- release.config.js
+++ release.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   plugins: [
-    // 1. コミット履歴から次期バージョン番号を算出する。
     [
 
       "@semantic-release/commit-analyzer",
@@ -9,7 +8,6 @@ module.exports = {
 
     ],
 
-    // 2. リリースノートのためのコンテンツ（テキスト）を生成する。
     [
 
       "@semantic-release/release-notes-generator",
@@ -18,15 +16,10 @@ module.exports = {
 
     ],
 
-    // 3. "2" で生成されたリリースノート用コンテンツを Changelog ファイルに記述する。
     '@semantic-release/changelog',
 
-    // 4-a. package.json の version フィールドを次期バージョン番号で更新する。
-    // 4-b. npmjs にパッケージを公開する。
-    // 4-c. npm dist-tag コマンドを実行して npmjs で公開するパッケージにタグを追加する。
     '@semantic-release/npm',
 
-    // 5. リリースフローで発生したアセットの更新差分をリポジトリにコミットする。
     [
       '@semantic-release/git',
       {
@@ -34,7 +27,6 @@ module.exports = {
       },
     ],
 
-    // 6. "2" で生成されたリリースノート用コンテンツを用いて GitHub Release を追加する。
     '@semantic-release/github',
   ],
 };
